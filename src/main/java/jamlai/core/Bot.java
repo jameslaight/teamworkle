25324d6570abb406a3003052fb2ad309016a33fe
jameslaight <150948866+jameslaight@users.noreply.github.com>
2023-11-20 23:54:00 +0000

    add intents for messaging and emoji use

--- src/main/java/jamlai/core/Bot.java
+++ src/main/java/jamlai/core/Bot.java
@@ -3,6 +3,7 @@ package jamlai.core;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.jetbrains.annotations.NotNull;
 
 public class Bot {
@@ -11,7 +12,9 @@ public class Bot {
 
 	public void build(@NotNull String token) {
 		JDABuilder builder = JDABuilder.createDefault(token);
-		builder.setActivity(Activity.playing("Teamwordle"));
+		builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_EMOJIS_AND_STICKERS);
+		builder.addEventListeners(this);
+		builder.setActivity(Activity.playing("Teamworkle"));
 		try {
 			jda = builder.build().awaitReady();
 		} catch (InterruptedException e) {
