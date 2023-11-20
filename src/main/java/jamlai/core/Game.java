cca7729f4daeff82c6b1f42b175d704f785f6cc6
jameslaight <150948866+jameslaight@users.noreply.github.com>
2023-11-20 13:26:58 +0000

    add embed used for displaying the board

--- src/main/java/jamlai/core/Game.java
+++ src/main/java/jamlai/core/Game.java
@@ -1,5 +1,7 @@
 package jamlai.core;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -21,6 +23,22 @@ public class Game {
 		Arrays.fill(board, line); //fill board with blank lines to begin
 
 		guessesLeft = maxGuesses;
+	}
+
+	public MessageEmbed getEmbed() {
+		EmbedBuilder builder = new EmbedBuilder();
+
+		StringBuilder strBuilder = new StringBuilder();
+		boolean first = true;
+		for (String line : board) {
+			if (first) first = false;
+			else strBuilder.append("\n");
+
+			strBuilder.append(line);
+		}
+		builder.addField("Board", strBuilder.toString(), false);
+
+		return builder.build();
 	}
 
 }
