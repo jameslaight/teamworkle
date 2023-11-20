07e993d1f3ac5bfabf68f694bb78a1cb62fc00a8
jameslaight <150948866+jameslaight@users.noreply.github.com>
2023-11-20 19:36:07 +0000

    add guess method

--- src/main/java/jamlai/core/Game.java
+++ src/main/java/jamlai/core/Game.java
@@ -5,12 +5,16 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
 
 	private final @NotNull String solution;
+	private final Map<Character, Integer> characterCounts = new HashMap<>();
+
 	private final int maxGuesses;
-	private int guessesLeft;
+	private int guesses = 0;
 
 	private final String[] board;
 
@@ -18,11 +22,41 @@ public class Game {
 		this.solution = solution;
 		this.maxGuesses = maxGuesses;
 
+		for (char c : solution.toCharArray()) { //count # of characters in solution
+			characterCounts.put(c, characterCounts.getOrDefault(c, 0) + 1);
+		}
+
 		board = new String[maxGuesses];
 		String line = LetterEmoji.getBlankEmoji().repeat(solution.length());
 		Arrays.fill(board, line); //fill board with blank lines to begin
+	}
 
-		guessesLeft = maxGuesses;
+	public boolean guess(String guess) {
+		StringBuilder builder = new StringBuilder(); //builder for the line on the board
+
+		Map<Character, Integer> counts = new HashMap<>(characterCounts);
+
+		for (int i = 0; i < solution.length(); i++) {
+			char c = guess.charAt(i);
+
+			LetterEmoji.Type letterType = LetterEmoji.Type.INCORRECT; //assume letter is incorrect
+			int count = counts.getOrDefault(c, 0);
+			if (count > 0) { //if the character is in the solution's character counts
+				if (solution.charAt(i) == c) { //if character is an exact match, it is solved
+					letterType = LetterEmoji.Type.SOLVED;
+				} else { //otherwise, it is misplaced
+					letterType = LetterEmoji.Type.MISPLACED;
+				}
+
+				counts.put(c, --count); //character count is decremented to display correct number of misplaced letters
+			}
+
+			builder.append(LetterEmoji.getEmoji(c, letterType)); //get the appropriate emoji and append to line
+		}
+
+		board[guesses++] = builder.toString();
+
+		return guess.equals(solution); //returns whether solved
 	}
 
 	public MessageEmbed getEmbed() {
