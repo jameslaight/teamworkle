cdd44c73d93411df2d798b0fedc28923d72ffd6e
jameslaight <150948866+jameslaight@users.noreply.github.com>
2023-11-21 03:52:37 +0000

    set up slash command basics

--- src/main/java/jamlai/core/Bot.java
+++ src/main/java/jamlai/core/Bot.java
@@ -4,13 +4,17 @@ import jamlai.util.Dictionary;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.EventListener;
 import java.util.List;
 import java.util.Random;
 
-public class Bot {
+public class Bot extends ListenerAdapter {
 
 	private JDA jda;
 
@@ -18,14 +22,22 @@ public class Bot {
 
 	public void build(@NotNull String token) {
 		JDABuilder builder = JDABuilder.createDefault(token);
+
 		builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_EMOJIS_AND_STICKERS);
 		builder.addEventListeners(this);
 		builder.setActivity(Activity.playing("Teamworkle"));
+
 		try {
 			jda = builder.build().awaitReady();
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
+
+		jda.updateCommands().addCommands(
+				Commands.slash("guess", "Guess a word in the daily teamworkle.")
+						.setGuildOnly(true)
+						.addOption(OptionType.STRING, "word", "word to guess", true, false)
+		).queue();
 	}
 
 	public void start() {
